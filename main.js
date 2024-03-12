094c546d537d1b64bb98c65b6a6d49520efa5143
RafaelErasto <162719292+RafaelErasto@users.noreply.github.com>
2024-03-12 11:24:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://a.espncdn.com/photo/2021/0827/r901184_1296x729_16-9.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://s2-ge.glbimg.com/7q5PwLluic5izS1EFmS4PEQ0qr8=/0x0:510x346/600x0/smart/filters:gifv():strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2020/P/L/GH725aRLawHtZvCwGQRw/912-cristiano-ronaldo-real-madrid-champons-league-getty.jpg";
+}
